5fdb4ffd9a2031aed962094cec45a4a821cdf233
Sravani <sravanibhumireddy2407@gmail.com>
2026-09-21 23:40:59 +0530

    sdsd232334

--- tests/LoginPage.spec.ts
+++ tests/LoginPage.spec.ts
@@ -29,7 +29,7 @@ expect.soft(await homePage.isLogoutLinkExist()).toBeTruthy();
 
 test('Verifyting Login Error Message', async ({ page }) => {
 await loginPage.doLogin('dev123@nal.com23','Test@123');
-console.log("hi hello");
+console.log("hi hello123");
 expect.soft(await loginPage.isInvalidLoginErrorDisplayed()).toBeTruthy();
 });
 
